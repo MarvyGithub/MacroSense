f52b41fae8b406bda851dfc52916fe81bb8bc5fa
Marvy <aremumarvelous@gmail.com>
2026-05-29 05:30:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1048,9 +1048,9 @@ col1, col2 = st.columns(2)
 
 col1.markdown("""
 **Connect With Me**
-- LinkedIn: [Your LinkedIn URL](your linkedin url here)
-- GitHub: [Your GitHub URL](your github url here)
-- Email: your.email@gmail.com
+- LinkedIn: [LinkedIn URL](https://www.linkedin.com/in/gideon-marvelous-5a19b919b?utm_source=share_via&utm_content=profile&utm_medium=member_android)
+- GitHub: [GitHub URL](https://github.com/MarvyGithub)
+- Email: aremumarvelous@gmail.com
 """)
 
 col2.markdown("""
